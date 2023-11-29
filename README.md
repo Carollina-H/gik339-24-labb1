57c75f67fd1536a2ba493c9614be69522d3f099d
Carollina-H <150289028+Carollina-H@users.noreply.github.com>
2023-11-29 15:24:51 +0100

    small changes <3

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gik339-24-labb1
- 
+This code is pretty- 
+
+SUPERCALIFRAGILISTICEXPIALITOCIOUS 
+
+... if you ask me ;D
+
+// Grupp 24
--- script.js
+++ script.js
@@ -1,97 +1,40 @@
-// const buttons = document.getElementsByTagName('button');
-//const inputFields = document.querySelectorAll('.input-field');
-
-// console.log(buttons);
-// console.log(inputFields);
-
-// inputFields.forEach((field) => field.addEventListener('blur', handleBlur));
-
-// for (let i = 0; i < buttons.length; i++) {
-//   buttons[i].addEventListener('click', handleClick);
-// }
-
-// //const dynamicDisplay = document.getElementById('dynamicDisplay');
-
-// // genererar block
-// function handleClick(e) {
-//   e.preventDefault();
-//   console.log('handleClick, eventobjekt', e);
-//   const colorField = settings.color;
-//   const contentField = settings.content;
-
-//   const newElement = document.createElement('div');
-//   newElement.classList.add('new-element');
-//   newElement.style.backgroundColor = colorField.value;
-//   newElement.innerHTML = contentField.value;
-
-//   dynamicDisplay.insertAdjacentElement('beforeend', newElement);
-// }
-
-// const output = document.getElementById('output');
-// function handleBlur(e) {
-//   console.log('handleBlur, eventobjekt', e);
-//   const name = e.target.name;
-//   const value = e.target.value;
-
-//   const html = `<p onclick="handleClick(event) class="test">Fältet ${name} har värdet ${value}</p>`;
-
-//   output.insertAdjacentHTML('afterbegin', html);
-// }
-
-
-// ------------------------------------------------------------
-
+// Uppgift 4
 const checkBox = document.getElementById('divStyle');
 const textFields = document.querySelectorAll('.textfield');
-const buttons = document.getElementsByTagName('button');
+const deleteBtn = document.getElementById('removeElement');
 const divElement = document.querySelector('.dynamicDiv');
 
 console.log(checkBox);
 console.log(textFields);
-console.log(buttons);
+console.log(deleteBtn);
 console.log(divElement);
 
-textFields.forEach((field) => field.addEventListener('input', handleInput));
+// Uppgift 5
 function handleInput(e) {
-    console.log('handleInput, eventobjekt', e);
-    //const inputColor = ;
-    //const inputContent = ;
-    const colorValue = e.target.name;
-    const contentValue = e.target.name;
-    console.log(content);
-    //let text = content;
-    //const text = '<p oninput="handleInput()" class="test"> ${content} </p>';
-    //console.log(divElement);
-    divElement.innerHTML = contentValue;
-    //`<p oninput="handleInput()" class="test"> ${content} </p>`
-  
+    console.log(handleInput);
+    if (e.target.name === 'content') { 
+      const contentText = e.target.value;
+      divElement.innerHTML = contentText; 
+    }
 }
 
-// function handleBlur(e) {
-//   console.log('handleBlur, eventobjekt', e);
-//   //const color = e.target.color;
-//   const content = e.target.content;
+// Uppgift 6
+// Checkboxen
 
-//   const html = `<p onclick="handleClick(event) class="test">${content}</p>`;
-//   divElement.insertAdjacentHTML('afterbegin', html);
-// }
+checkBox.addEventListener('change', function() {
+  console.log("Colored divElement!")
+  for (let textfield of textFields) {
+    if (textfield.name === 'color') {
+      divElement.style.background = textfield.value;
+    }
+  }
+});
 
-function handleClick(e) {
-  e.preventDefault();
-  console.log('handleClick, eventobjekt', e);
-  const colorField = settings.color;
-  const contentField = settings.content;
-  divElement.style.backgroundColor = colorField.value;
-  divElement.innerHTML = contentField.value; 
-}
+// Textfälten
+textFields.forEach((field) => field.addEventListener('input', handleInput));
 
-
-/*
-Variabeln contentValue fungerar inte som den ska, specifiera att det ska vara conent.
-Just nu fungerar funktionen/variabeln på både content och color.
-
-Kan behöva ha for loop istället
-Anteckning från L2, kan behöva switcha till sibling i input? 
-console.log(section.firstElementChild.nextElementSibling.previousElementSibling.parentElement); //kan dyka in o komma tbx gsk mkt
-
-*/
+// Knappen
+deleteBtn.addEventListener('click', function() {
+  console.log("Removed divElement!")
+  divElement.remove();
+});
